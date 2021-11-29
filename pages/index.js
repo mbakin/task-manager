43a2a9143bd17cabcd295d72dd0c9ebd643b844e
mbakin <mehmetberkakinn@outlook.com>
2021-11-29 18:43:23 +0300

    create page design

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 
-
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Next.js</h1>
-      </div>
+    <div className="h-screen flex">
+        <div className="bg-white w-64 px-8 py-10 border-r border-blue-500 border-opacity-25 rounded-lg">
+          <div>
+            <img src="/images/logo.png" alt="logo" className="w-10 h-10" />
+          </div>
+          <nav className="mt-8">
+            <h3 className="text-md font-semibold text-gray-700 uppercase tracking-wide">Scrumboard</h3>
+            <div className="mt-2 -mx-4">
+              <a href="#" className="flex justify-between items-center px-4 py-2">
+                <span className="text-sm font-medium text-gray-500">Backlog</span>
+                <span className="text-sm font-medium text-gray-500">17</span>
+              </a>
+            </div>
+          </nav>
+        </div>
+
+
+        <div className="flex-1 min-w-0">Main Content</div>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'light': '#ffffff',
+      'sidebar-text-color': '#5EEAD4',
+      'sidebar-background-color': '#EAE7AF',
+      }
+  },
   },
   variants: {
     extend: {},
