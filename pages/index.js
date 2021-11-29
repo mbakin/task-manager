91d6b9593386a85af529f83b824e18c796fa9ff3
mbakin <mehmetberkakinn@outlook.com>
2021-11-29 18:59:59 +0300

    update sidebar

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,41 @@ export default function Home() {
             <img src="/images/logo.png" alt="logo" className="w-10 h-10" />
           </div>
           <nav className="mt-8">
-            <h3 className="text-md font-semibold text-gray-700 uppercase tracking-wide">Scrumboard</h3>
+            <h3 className="text-md font-semibold text-gray-500 uppercase tracking-wide">Scrumboard</h3>
             <div className="mt-2 -mx-4">
               <a href="#" className="flex justify-between items-center px-4 py-2">
-                <span className="text-sm font-medium text-gray-500">Backlog</span>
-                <span className="text-sm font-medium text-gray-500">17</span>
+                <span className="text-sm font-medium text-gray-700">Backlog</span>
+                <span className="text-xs font-semibold text-gray-700">17</span>
+              </a>
+              <a href="#" className="flex justify-between bg-gradient-to-r from-purple-800 to-blue-500 rounded-lg items-center px-4 py-2 ">
+                <span className="text-sm font-medium text-white">Active Sprints</span>
+                <span className="text-xs font-semibold text-white">1</span>
+              </a>
+              <a href="#" className="flex justify-between items-center px-4 py-2">
+                <span className="text-sm font-medium text-gray-700">Reports</span>
+                <span className="text-xs font-semibold text-gray-700">11</span>
+              </a>
+            </div>
+
+            <h3 className="text-md mt-8 font-semibold text-gray-500 uppercase tracking-wide">Tags</h3>
+            <div className="mt-2 -mx-4">
+              <a href="#" className="flex justify-between items-center px-4 py-2">
+                <span className="text-sm font-medium text-gray-700">Bug</span>
+              </a>
+              <a href="#" className="flex justify-between items-center px-4 py-2 ">
+                <span className="text-sm font-medium text-gray-700">New Feature</span>
+              </a>
+              <a href="#" className="flex justify-between items-center px-4 py-2">
+                <span className="text-sm font-medium text-gray-700">Marketing</span>
+              </a>
+              <a href="#" className="flex justify-between items-center px-4 py-2">
+                <span className="text-sm font-medium text-gray-700">Design</span>
+              </a>
+              <a href="#" className="flex justify-between items-center px-4 py-2">
+                <span className="text-sm font-medium text-gray-700">Research</span>
+              </a>
+              <a href="#" className="flex justify-between items-center px-4 py-2">
+                <span className="text-sm font-medium text-gray-700">Overdue</span>
               </a>
             </div>
           </nav>
